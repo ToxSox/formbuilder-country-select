ba2db308f6850042a8063bab8f1dca8f6fca5463
Thomas Sojka <github@tsojka.com>
2024-04-18 11:49:02 +0200

    Update fcs.js

--- fcs.js
+++ fcs.js
@@ -1,17 +1,6 @@
 (function(){
 	window.addEventListener('load', function(){
 		console.log("setTimeout");
-		setTimeout(function() {
-			var queryString = window.location.search;
-			var urlParams = new URLSearchParams(queryString);
-			var language = urlParams.get('prefill_language');
-		
-			console.log(language);
 
-			languageSelect = document.querySelectorAll("[data-type='language-selector']")[0].children[1].children[0];
-			languageSelect.value = "de";
-			languageSelect.dispatchEvent(new Event('change'));
-		
-		}, 1000);
 	});
 });
