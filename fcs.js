f37030716f331c0168601c3cd715ff8a3951ac37
Thomas Sojka <github@tsojka.com>
2024-04-18 11:34:59 +0200

    Update fcs.js

--- fcs.js
+++ fcs.js
@@ -1,18 +1,20 @@
 (function(){
 window.addEventListener('load', function(){
-	setTimeout(function() {
-var queryString = window.location.search;
-var language = urlParams.get('prefill_language');
+setTimeout(function() {
+	var queryString = window.location.search;
+	var language = urlParams.get('prefill_language');
 
-		console.log(language);
+	console.log(language);
 
 		
 	languageSelect = document.querySelectorAll("[data-type='language-selector']")[0].children[1].children[0];
 	languageSelect.value = "de";
 	languageSelect.dispatchEvent(new Event('change'));
 
-}, 1000);}
-
+}, 1000);
 });
-	
+
+loader.engine.document.addEventListener('load', function(){
+console.log("engine load");
+
 })();
