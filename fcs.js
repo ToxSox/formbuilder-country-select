6372695ebe6217e4573091feafa1c8446d01d06b
Thomas Sojka <github@tsojka.com>
2024-04-18 10:46:02 +0200

    Update fcs.js

--- fcs.js
+++ fcs.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
 	console.log("TEST");
 	languageSelect = document.querySelectorAll("[data-type='language-selector']")[0].children[1].children[0];
 	languageSelect.value = "de";
-	languageSelect.value.dispatchEvent(new Event('change'));
+	languageSelect.dispatchEvent(new Event('change'));
 
 
 });
